49638a248da977798009767103c32b9746e0a153
appledailyarchive <appledailyarchive@protonmail.com>
2021-07-25 17:44:38 +0800

    style: update about us style

--- src/components/Markdown.tsx
+++ src/components/Markdown.tsx
@@ -12,18 +12,21 @@ export const Markdown: React.FC = ({ children }) => (
       img: Image,
       h1: p => <Heading my={6} as="h1" {...p} />,
       h2: p => <Heading my={4} as="h2" size="lg" {...p} />,
-      h3: p => <Heading as="h3" size="md" my={2} {...p} />,
+      h3: p => <Heading as="h3" size="md" mt={4} mb={2} {...p} />,
       h4: p => <Heading as="h4" size="sm" my={2} {...p} />,
       h5: p => <Heading as="h5" size="xs" {...p} />,
       ol: p => <chakra.ol sx={{ listStylePosition: 'inside' }} {...p} />,
       p: p => <Text as="p" mb={2} {...p} />,
       a: p => (
         <chakra.a
-          color="theme.200"
+          color="theme.400"
+          borderColor="theme.400"
+          borderBottomWidth="1px"
+          borderStyle="dotted"
           _hover={{
-            borderBottomWidth: '1px',
-            borderStyle: 'dotted',
-            borderColor: 'theme.600',
+            color: 'theme.100',
+            borderBottomWidth: '2px',
+            borderColor: 'theme.100',
           }}
           {...p}
         />
