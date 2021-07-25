ece24659a9287ba70b22e8b30b16b4f46dbfb14e
appledailyarchive <appledailyarchive@protonmail.com>
2021-07-25 09:22:51 +0800

    feat(experimental): remove image loading spinner since Cloudflare enabled image optimization
    Since we enabled Image Resizing, Polish and Mirage in Cloudflare. It
    might be best if we remove the custom loading state spinner for images
    with just plain fallback instead.
    
    Note: This commit is experimental. We will observe the effect on dev
    first before deploy to production. We may revert.

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -1,48 +1,28 @@
-import React, { useState } from 'react'
-import { Box, Flex, Image as ChakraImage, ImageProps, Spinner } from '@chakra-ui/react'
+import React from 'react'
+import { Box, Flex, Image as ChakraImage, ImageProps } from '@chakra-ui/react'
 
 import { Empty } from './Empty'
-import { isNil } from 'ramda'
-import { useUpdateEffect } from 'react-use'
 
 export const ArticleImage: React.FC<ImageProps> = ({ onLoad, src, onError, ...props }) => {
-  const [loading, setLoading] = useState(!isNil(src))
-
-  useUpdateEffect(() => {
-    setLoading(true)
-  }, [src])
-
   const handleImageLoad: ImageProps['onLoad'] = event => {
-    setLoading(false)
     onLoad && onLoad(event)
   }
 
   const handleError: ImageProps['onError'] = event => {
-    setLoading(false)
     onError && onError(event)
   }
 
   return (
-    <ChakraImage
-      {...props}
-      src={src}
-      onLoad={handleImageLoad}
-      onError={handleError}
-      fallback={<ImageFallback loading={loading} />}
-    />
+    <ChakraImage {...props} src={src} onLoad={handleImageLoad} onError={handleError} fallback={<ImageFallback />} />
   )
 }
 
-const ImageFallback: React.FC<{ loading: boolean }> = ({ loading }) => {
+const ImageFallback: React.FC = () => {
   return (
     <Flex h={333} w="full" justifyContent="center" alignItems="center">
-      {loading ? (
-        <Spinner size="lg" color="theme.500" speed="0.9s" />
-      ) : (
-        <Box w="full" h="full">
-          <Empty />
-        </Box>
-      )}
+      <Box w="full" h="full">
+        <Empty />
+      </Box>
     </Flex>
   )
 }
