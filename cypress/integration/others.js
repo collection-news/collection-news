570e6bac4fceb1e756e2f4e3ae01d8ef2318eb6c
appledailyarchive <appledailyarchive@protonmail.com>
2022-04-22 09:49:28 +0800

    fix: should show search box when nav from menu

--- cypress/integration/others.js
+++ cypress/integration/others.js
@@ -6,6 +6,12 @@ describe('Other pages', () => {
     cy.get('.gsc-control-cse').should('exist')
   })
 
+  specify('Should show search box when navigate from menu', () => {
+    cy.visit('/')
+    cy.get('[data-cy=header-search-btn]').click()
+    cy.get('.gsc-control-cse').should('exist')
+  })
+
   specify('Should load sitemap from API', () => {
     cy.request('/sitemap/appledaily/20210623.xml').then(resp => {
       expect(resp.status).to.eq(200)
