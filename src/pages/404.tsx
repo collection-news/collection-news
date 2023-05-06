ac08f554225c66e4ebdc4749e058cfe40cec41a7
appledailyarchive <appledailyarchive@protonmail.com>
2023-05-06 18:24:53 +0800

    fix: migrate to new next/image

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,5 +1,5 @@
 import { Box, Button, Center, Flex, Heading } from '@chakra-ui/react'
-import Image from 'next/legacy/image'
+import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import logoError from '../assets/logoError.svg'
@@ -10,7 +10,16 @@ const Custom404Page = () => (
     <NonArticleHead title="找不到 | 聞庫" />
     <Center>
       <Box justifyContent="center" py={10} my={50}>
-        <Image src={logoError} alt="404icon" width={500} height={120} />
+        <Image
+          src={logoError}
+          alt="404icon"
+          width={500}
+          height={120}
+          style={{
+            maxWidth: '100%',
+            height: 'auto',
+          }}
+        />
         <Heading textAlign="center">404 Not Found</Heading>
         <Flex justifyContent="center" mt="4">
           <Link href="/" passHref>
