6bc758c4d92bc5ac7b696762e276824323168b7c
appledailyarchive <appledailyarchive@protonmail.com>
2024-06-15 11:33:45 +0800

    ci: add pm2 deployment

---
+++ ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  apps: [
+    {
+      name: 'nextjs',
+      script: 'npm',
+      cwd: '/var/www/collection-news/',
+      args: 'start',
+      error_file: '/dev/null',
+      out_file: '/dev/null',
+    },
+  ],
+}
